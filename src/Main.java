2f5a983e69091c1097a0fe9c4aae434ac2b5b08d
Дмитрий <dimvik007@gmail.com>
2026-05-29 21:35:46 +1000

    Задание 1 дополнил

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Задание 1");
         int age = 17;
         if (age >= 18) {
-            System.out.println("Если возвраст человека 18, то он совершенолетний");
+            System.out.println("Если возвраст человека 18 и более, то он совершенолетний");
         } else if (age < 18) {
             System.out.println("Не достиг совершеннолетия, нужно немного подождат");
         }
