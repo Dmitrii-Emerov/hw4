c0aca639a8c2b5a08f463a2950f73bfa4ff6c6d2
Дмитрий <dimvik007@gmail.com>
2026-05-29 21:34:42 +1000

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        int age = 32;
-        boolean canNotGoToSchool = age < 7 || age > 18;
-        if (age >= 18 && age < 21) {
-            System.out.println("Ты достаточно взрослый, чтобы водить,но недостаточно, взрозлый чтобы пить алкоголь");
-        } else if (age >= 21) {
-            System.out.println("Ты можешь пить алкоголь и водить машину");
+        System.out.println("Домашнне задание");
+        System.out.println("Задание 1");
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Если возвраст человека 18, то он совершенолетний");
+        } else if (age < 18) {
+            System.out.println("Не достиг совершеннолетия, нужно немного подождат");
         }
     }
 }
